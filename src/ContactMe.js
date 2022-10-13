db8b7db9cc0af1dc2483c45e02d1a266c95d0fe3
komisssarova <komisssarova.uk@gmail.com>
2022-10-13 13:13:35 +0100

    upgraded  contact page

--- src/ContactMe.js
+++ src/ContactMe.js
@@ -5,6 +5,10 @@ import "./ContactMe.css";
 export default function ContactMe() {
   return (
     <div className="ContactMe">
+      <h5>Let's get in touch!</h5>
+      <p>
+        komisssarova.uk@gmail.com 
+      </p>
     </div>
   );
 }
--- src/Projects.js
+++ src/Projects.js
@@ -6,24 +6,28 @@ export default function Projects() {
   return (
     <div className="Projects">
       <div className="row justify-content-between">
-        <h1>Check out my projects</h1>
+        <h3>Check out my projects</h3>
         
         <div className="col-4">
           <img src="weather.png" alt="" />
+          <p>Weather App built with React</p>
           <a
             href="https://endearing-churros-ff3e12.netlify.app/"
             className="link-secondary"
           >
-            Weather App
+            Learn more
           </a>
         </div>
         <div className="col-4">
           <img src="dictionary.png" alt="" />
+          <p>
+            Dictionary App built with React
+          </p>
           <a
             href="https://taupe-stardust-dd3019.netlify.app/"
             className="link-secondary"
           >
-            Dictionary App
+            Learn more
           </a>
           </div>
         </div>
