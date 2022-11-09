06d52a6eb0411c8f84f06be4f7f52706c0dfaf21
komisssarova <komisssarova.uk@gmail.com>
2022-11-09 18:21:51 +0000

    styled About component

--- src/About.jsx
+++ src/About.jsx
@@ -1,19 +1,38 @@
 import React from "react";
 import "./about.css";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 export default function About() {
   return (
     <section id="about">
-      <div className="row justify-content-between">
-        <div className="col-4"></div>
-        <div className="col-4">
-          <p className="text">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about-container">
+        <div className="about-me">
+          <div className="about-me-image">
+            <img src="cvpicture.jpg" alt="img of kateryna"></img>
+          </div>
+        </div>
+
+        <div className="about-contant">
+          <div className="about-cards">
+            <article className="about-card">
+              <VscFolderLibrary className="about-icon" />
+              <h5>Projects</h5>
+              <small>2 Completed</small>
+            </article>
+          </div>
+
+          <p>
             A determined Junior Front-End Developer with a passion for clean
             code and creative solutions. Proficient in React, ES6+, HTML/CSS
             with a solid knowledge base of utilising Bootstrap and Visual Code.
             In search of an Entry-level Front-End Developer position with the
             opportunity to work and grow within an amazing team.
           </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
         </div>
       </div>
     </section>
--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -3,32 +3,5 @@ import "./App";
 import "./portfolio.css";
 
 export default function Portfolio() {
-  return (
-    <section id="portfolio">
-      <div className="row justify-content-between">
-        <h3>Check out my projects</h3>
-
-        <div className="col-4">
-          <img src="weather.png" alt="" />
-          <p>Weather App built with React</p>
-          <a
-            href="https://endearing-churros-ff3e12.netlify.app/"
-            className="link-secondary"
-          >
-            Learn more
-          </a>
-        </div>
-        <div className="col-4">
-          <img src="dictionary.png" alt="" />
-          <p>Dictionary App built with React</p>
-          <a
-            href="https://taupe-stardust-dd3019.netlify.app/"
-            className="link-secondary"
-          >
-            Learn more
-          </a>
-        </div>
-      </div>
-    </section>
-  );
+  return <section id="portfolio"></section>;
 }
