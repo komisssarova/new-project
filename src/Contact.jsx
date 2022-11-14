13b432a22646d9fee9b8e7d39cafe7bb1b934817
komisssarova <komisssarova.uk@gmail.com>
2022-11-14 19:19:55 +0000

    added contact component

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,12 +1,64 @@
 import React from "react";
-import "./App";
 import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { AiFillLinkedin } from "react-icons/ai";
+import { AiOutlineInstagram } from "react-icons/ai";
 
-export default function Contact() {
+const Contact = () => {
   return (
     <section id="contact">
       <h5>Let's get in touch!</h5>
-      <p>komisssarova.uk@gmail.com</p>
+      <h2>Contact Me</h2>
+
+      <div className="container contact-container">
+        <div className="contact-options">
+          <article className="contact-option">
+            <MdOutlineEmail className="contact-option-icon" />
+            <h4>Email</h4>
+            <a href="mailto:komisssarova.uk@gmail.com" target="_blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact-option">
+            <AiFillLinkedin className="contact-option-icon" />
+            <h4>LinkedIn</h4>
+            <a
+              href="https://www.linkedin.com/in/kateryna-komisarova/"
+              target="_blank"
+            >
+              Get in touch
+            </a>
+          </article>
+          <article className="contact-option">
+            <AiOutlineInstagram className="contact-option-icon" />
+            <h4>Instagram</h4>
+            <a href="https://www.instagram.com/komisssarova/" target="_blank">
+              Get in touch
+            </a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
     </section>
   );
-}
+};
+
+export default Contact;
